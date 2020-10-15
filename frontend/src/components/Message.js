368286c50e6f524b64ca2022e09864d7a12df115
patricia <patriciapavia236@gmail.com>
2020-10-15 13:14:49 +1100

    created loader and message component

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Alert, alert } from 'react-bootstrap';
+
+const Message = ({ variant, children }) => {
+	return <Alert variant={variant}>{children}</Alert>;
+};
+
+Message.defaultProps = {
+	variant: 'info'
+};
+
+export default Message;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,11 +2,13 @@ import React, { useEffect }from "react";
 import { useDispatch, useSelector  } from 'react-redux'
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 
 
-const HomeScreen = () => {
+       const HomeScreen = () => {
   const dispatch = useDispatch()
 
   const productList =useSelector(state => state.productList) // productList from store
@@ -22,7 +24,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Lastest Products </h1>
-  { loading ?  (<h2>Loading</h2>) :  error ?(<h3>{error} </h3>) : (<Row>
+  { loading ?  (<Loader/>) :  error ?(<h3><Message variant='danger'>{error}</Message> </h3>) : (<Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
